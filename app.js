6bd9a53a0fafc503092541ae56df396f5137c69e
Aubrey Winkie <aubreywinkie@utexas.edu>
2017-12-11 12:27:47 -0600

    finished ls

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var office = ["Michael Scott", "Dwight Schrute", "Jim Halpert", "Creed Bratton", "Stanley Hudson" ];
-$("#buttons").append(office);
-var authKey
-function displayGif() {
-	$('#buttons').on("click", function()) {
 
-	var character = $(this).atrr("data-name");
+window.onload = (function() {
+
+var office = ["Michael Scott", "Dwight Schrute", "Jim Halpert", "Creed Bratton", "Stanley Hudson" ];
+// $("#buttons").append(office);
+var authKey =  "YsohIxnEqrKiWeq8gEndUXsWFjWLN0m1&limit=5"
+
+$('#buttons-view').on("click", ".character", function() {
+		console.log("click");
+	var character = $(this).attr("data-name");
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + character + "&api_key=YsohIxnEqrKiWeq8gEndUXsWFjWLN0m1&limit=5"
 
 	$.ajax({
@@ -12,17 +15,17 @@ function displayGif() {
 		method: "GET"
 	}).done(function(response) {
 		var results = response.data;
-
+		console.log(results);
 		for (var i = 0; i< results.length; i++) {
+			// $("#gifs-appear-here").empty(); trying to get a new set of 5 images each time you click but not working
 			var gifDiv = $("<div class = 'item'>")
-			var personImage = $("<img>");
+			var personImage = $("<img class='gif'>");
 			personImage.attr("src", results[i].images.fixed_height.url);
-
+			personImage.attr("data-still", results[i].images.fixed_height_still.url);
 			gifDiv.prepend(personImage);
 			$("#gifs-appear-here").prepend(gifDiv);
 		}
 		
-	});
 //another way to do it 
 // response.data.map(result=>$('#gifs-appear-here').prepend(`
 // 	<div class= "item">
@@ -33,7 +36,7 @@ function displayGif() {
 });
 
 
-
+})
 function renderButton() {
 
 	$("#buttons-view").empty();
@@ -41,7 +44,7 @@ function renderButton() {
 	for (var i =0; i < office.length; i++) {
 		var a = $("<button>");
 		a.addClass("character");
-		a.atrr("data-name", office[i]);
+		a.attr("data-name", office[i]);
 		a.text(office[i]);
 		$("#buttons-view").append(a);
 	}
@@ -49,12 +52,17 @@ function renderButton() {
 }
 
 $("#add-character").on("click", function(event) {
+	console.log("click");
 	event.preventDefault();
 	var character = $("#character-input").val().trim();
 	office.push(character);
 	renderButton();
 });
 
-$(document).on("click", ".character", displayGif);
+$("#gifs-appear-here").on("click", ".gif", function(event) {
+	console.log("click");
+ var still = $(this).attr("data-still");
+ $(this).attr("src", still);
+})
 renderButton();
-
+})
